9181b487ac5723575b99e163d0ff1056e1360abd
Anupam Sharma <as9254794@gmail.com>
2023-11-17 18:00:49 +0530

    adding new file

---
+++ script.js
@@ -0,0 +1,50 @@
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+
+}
+loco()
+
+
+
+gsap.from("#icon-floating-div-up i",{
+    y:100,
+    duration:7,
+    repeat:-1,
+    yoyo:true,
+
+
+})
+gsap.from("#icon-floating-div-down i",{
+    y:-100,
+    duration:10,
+    delay:2,
+    repeat:-1,
+    yoyo:true,
+
+
+})
